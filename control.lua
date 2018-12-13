c83a407ef8b837694eb43ec3fed766ac81d6b501
H8UL <h8ul.mods@gmail.com>
2018-12-13 02:12:20 +0000

    major rewrite to mod tech tree, refs #3

--- control.lua
+++ control.lua
@@ -26,32 +26,23 @@ end
 
 local function onReseearchFinished(event)
     local eventForce = event.research.force
-    local currentReds = 0
-    local totalReds = 0
-    local equalTechValue = settings.global["research-causes-evolution-all-techs-equal"].value
+    local evolution = 0
 
     for _, tech in pairs(eventForce.technologies) do
-        if tech.research_unit_count_formula == nil and not tech.upgrade then -- not an infinite research and not an upgrade
-            for _,ingredient in pairs(tech.research_unit_ingredients) do
-                if ingredient.name == "science-pack-1" then -- find the red research pack
+        if tech.researched and tech.effects then
+            for _,effect in pairs(tech.effects) do
+                if effect.type == "nothing" and effect.effect_description then
 
-                    local thisTechReds
-                    if equalTechValue then
-                        thisTechReds = 1
-                    else
-                        thisTechReds = ingredient.amount * tech.research_unit_count
+                    if effect.effect_description[1] == "research-causes-evolution-effect" then
+                        local value = effect.effect_description[2]
+                        evolution = evolution + value
                     end
-
-                    if tech.researched then
-                        currentReds = currentReds + thisTechReds
-                    end
-                    totalReds = totalReds + thisTechReds
                 end
             end
         end
         for _,force in pairs(game.forces) do
             if force.ai_controllable then
-                force.evolution_factor = currentReds / totalReds
+                force.evolution_factor = evolution / 100.0
             end
         end
     end
